bf000db01691fbcd2279d4cecf7b4c9bd8bfd612
Nilson Lazarin <nilsonmori@yahoo.com>
2024-10-16 10:50:05 -0300

    atualizando repositório debian

--- dependencias_asROOT.sh
+++ dependencias_asROOT.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+echo 'deb [trusted=yes] http://bsi.cefet-rj.br/repo/~debian labredes main' > /etc/apt/sources.list
 echo 'deb http://deb.debian.org/debian/               bookworm                main contrib non-free non-free-firmware' >> /etc/apt/sources.list
 echo 'deb http://security.debian.org/debian-security  bookworm-security       main contrib non-free non-free-firmware' >> /etc/apt/sources.list
 echo 'deb http://deb.debian.org/debian/               bookworm-updates        main contrib non-free non-free-firmware' >> /etc/apt/sources.list
 echo 'deb http://deb.debian.org/debian/               bookworm-backports      main contrib non-free non-free-firmware' >> /etc/apt/sources.list
+
+
 
 sudo apt update
 
