13f9f17e523b60940842c9d36c95156fdda3de0b
Nilson Lazarin <nilsonmori@yahoo.com>
2024-10-16 10:39:15 -0300

    atualizando repositório debian

--- dependencias_asROOT.sh
+++ dependencias_asROOT.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+echo 'deb http://deb.debian.org/debian/               bookworm                main contrib non-free non-free-firmware' >> /etc/apt/sources.list
+echo 'deb http://security.debian.org/debian-security  bookworm-security       main contrib non-free non-free-firmware' >> /etc/apt/sources.list
+echo 'deb http://deb.debian.org/debian/               bookworm-updates        main contrib non-free non-free-firmware' >> /etc/apt/sources.list
+echo 'deb http://deb.debian.org/debian/               bookworm-backports      main contrib non-free non-free-firmware' >> /etc/apt/sources.list
+
+sudo apt update
 
 # Baixar o instalador do Composer
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
